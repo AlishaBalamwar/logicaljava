e04cdadbf3d72c7fedd9a8e1eaac67b8b13fa264
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-26 13:13:48 +0530

    its checks whether number is prime or not

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+package javaLogical;
+import java.util.*;
+public class PrimeNumber {
+	public static void main(String[]  args) {
+		int n;
+		int i =2;
+		System.out.println("Enter the number: n");
+		Scanner s =new Scanner(System.in);
+		n = s.nextInt();
+		boolean flag = false;
+		while(i<=n/2) {
+		if (n%i==0){
+		flag = true;
+		break;
+		}
+		++i;
+		}
+		if (!flag) {
+		System.out.println(+n +" " + "is a prime number");
+		}
+		else {
+		System.out.println(+n + " " + "is not a prime number");	
+		}
+		}
+}
